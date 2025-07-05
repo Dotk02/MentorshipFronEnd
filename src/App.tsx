b2c237dd604c7ee62de93e6c551b429237a3509a
Soderu Kolawole <soderukolawole@hotmail.com>
2025-07-05 23:28:31 +0100

    forntend setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className='bg-black text-white'>
+        WELCOME TO MENTORSHIP FRONTEND
+      </div>
+      </>
+  )
+}
+
+export default App
